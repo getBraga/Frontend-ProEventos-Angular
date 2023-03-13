1b85c1ab9118df1f0a611576e93a7cf0437cf42f
Ysmael Braga <ysmael.braga@ibm.com>
2023-03-12 23:51:54 -0300

    fazendo uma requisicao get para o backend

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-
-  constructor() { }
+  public eventos: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
   }
+public getEventos():void {
+  this.http.get("https://localhost:5001/api/eventos").subscribe(
+    response => this.eventos  = response,
+    error => console.log(error)
+  )
 
+}
 }
