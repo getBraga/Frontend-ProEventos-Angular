948700bf84b213843023b4236713edfdb11ddd33
Ysmael Braga <ysmael.braga@ibm.com>
2023-03-17 01:17:26 -0300

    feito toda a logica de filtrar eventos

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -11,7 +11,23 @@ export class EventosComponent implements OnInit {
   widthImg: number = 150;
   marginImg: number = 2;
   mostrarImagens: boolean = true;
-  filtrarLista:string = "";
+  public eventosFiltrados:any = [];
+  private _filtroLista:string = "";
+  public get filtroLista(): string {
+    return this._filtroLista
+  }
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    console.log(filtrarPor)
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)  !== -1||
+    evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
   constructor(private http: HttpClient) { }
 alterarImagem() {
   this.mostrarImagens = !this.mostrarImagens;
@@ -21,8 +37,13 @@ alterarImagem() {
   }
 public getEventos():void {
   this.http.get("https://localhost:5001/api/eventos").subscribe(
-    response => this.eventos  = response,
-    error => console.log(error)
+ {
+  next: (response) => {
+    this.eventos  = response
+    this.eventosFiltrados = response
+  },
+  error: error => console.log(error)
+ }
   )
 
 }
